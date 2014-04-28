b3d64fa0eb2f8dd27312b63faecae8cc894ab39f
ltebean <yucong1118@gmail.com>
2014-04-28 11:37:40 +0800

    done

--- public/js/search.js
+++ public/js/search.js
@@ -7,14 +7,17 @@ app.directive('ngEnter', function() {
 				scope.$apply(function() {
 					scope.$eval(attrs.ngEnter);
 				});
-
 				event.preventDefault();
 			}
 		});
 	};
 });
 
-function Controller($scope, $http) {
+function Controller($scope, $http, $timeout, $location, $anchorScroll) {
+
+	$scope.clearInput = function() {
+		$scope.searchInput = '';
+	}
 
 	$scope.search = function() {
 		var input = $scope.searchInput;
@@ -33,43 +36,72 @@ function Controller($scope, $http) {
 		$http.get('/rest/search?' + queryString)
 			.success(function(data) {
 				$scope.pkgs = data;
-				if(data.length>0){
+				if (data.length > 0) {
 					$scope.viewDetail(data[0])
+				} else {
+					$scope.pkg=null;
+					showMsg('No package found');
 				}
 			}).error(function(data) {
-				alert('error')
+				showMsg('Opps, unknown error');
 			})
 
 	}
 
-	searchByKeyWord('cortex')
+	searchByKeyWord('app')
 
 	function searchByCriteria(input) {
-		var queryObject={}
-		var field=input.split('\|');
+		var queryObject = {}
+		var field = input.split('\|');
 		for (var i = field.length - 1; i >= 0; i--) {
-			var query=field[i].split(':') //['keyword':'ajax']
-			queryObject[query[0]]=query[1]
+			var query = field[i].split(':') //['keyword':'ajax']
+			queryObject[query[0]] = query[1]
 		};
 
 		$http.get('/rest/search?' + toQueryString(queryObject))
 			.success(function(data) {
 				$scope.pkgs = data;
-				if(data.length>0){
+				if (data.length > 0) {
 					$scope.viewDetail(data[0])
+				}else {
+					$scope.pkg=null;
+					showMsg('No package found');
 				}
 			}).error(function(data) {
-				alert('error')
+				showMsg('Opps, unknown error');
 			})
+	}
+
+	$scope.marked = function(rawText) {
+		var result;
+		try{
+			result= marked(rawText);
+			return result;
+		}catch(e){
+			return result;
+		}
 	}
 
 	$scope.viewDetail = function(pkg) {
 		$http.get('/rest/package/' + pkg.name)
 			.success(function(data) {
 				$scope.pkg = data;
+				goToTop();
 			}).error(function(data) {
-				alert('error')
+				showMsg('Opps, unknown error');
 			})
+	}
+
+	function goToTop() {
+		$location.hash('top');
+		$anchorScroll();
+	}
+
+	function showMsg(msg) {
+		$scope.msg = msg;
+		$timeout(function() {
+			$scope.msg = '';
+		}, 3000)
 	}
 
 	function toQueryString(obj) {
